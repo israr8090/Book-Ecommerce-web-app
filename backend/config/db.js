bf16884e37af7bc606e7e78c28611c4e03579607
israr8090 <mdistata772@gmail.com>
2024-07-01 15:27:35 +0530

    added atlus mongodb

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
+import 'dotenv/config';  //--dotenv
+const atlas = process.env.DB_URL;
 
 const connectDB = async () => {
    try {
-    await mongoose.connect ('mongodb://localhost:27017/bookEcommerce');
-    console.log('server is connected with Database Successfully')
+   //  await mongoose.connect ('mongodb://localhost:27017/bookEcommerce');
+    await mongoose.connect(atlas);
+    console.log('server is connected with Database Successfully')  
    } catch (error) {
         console.log(error)
    }
